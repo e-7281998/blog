cbb8afef305b2550231b521667beda8474098b27
e-7281998 <jejjcs43@naver.com>
2022-04-05 14:30:40 +0900

    back-end(koa, nodemon), mongoose(rest api)

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,133 @@
+import Post from '../../models/post.js';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 404;   //bad request
+        return;
+    }
+    return next();
+}
+
+export const write = async ctx => {
+    const schema = Joi.object().keys({
+        //객체가 다음 필드를 가지고 있음을 검증
+        title: Joi.string().required(), //required()가 있으면 필수 항목
+        body: Joi.string().required(),
+        tags: Joi.array().items(Joi.string()).required() //문자열로 이루어진 배열
+    });
+
+    //검증하고 나서 검증 실패인 경우 에러처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+export const list = async ctx => {
+    //query는 문자열이기 때문에 숫자로 변환해 주어야 한다.
+    //값이 주어지지 않았다면 1을 기본으로 사용한다.
+    //parseInt 사용 시 두번째 인자를 넣지 않는다면
+    //parseInt("08") 사용 시 0으로 인식해 8진수로 가정하여 결과가 0이 나오게 된다.
+    const page = parseInt(ctx.query.page || '1', 10);
+    if (page < 1) {
+        ctx.status = 404;
+        return;
+    }
+    try {
+        const posts = await Post.find()
+            .sort({ _id: -1 })
+            .limit(10)
+            .skip((page - 1) * 10)
+            .exec();
+        const postCount = await Post.countDocuments().exec();
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
+        ctx.body = posts.map(post => post.toJSON()).map(post => ({
+            ...post,
+            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+        }));
+    } catch (e) {
+        console.log(e);
+    }
+};
+export const read = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, 5);
+    }
+};
+export const remove = async ctx => {
+    const { id } = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204;   //no content : 성공했지만 응답할 데이터는 없음
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+// export const deleteAll = async ctx => {
+//     try {
+//         await Post.remove({});
+//         ctx.status = 204;   //no content : 성공했지만 응답할 데이터는 없음
+//     } catch (e) {
+//         ctx.throw(500, e);
+//     }
+// };
+
+export const update = async ctx => {
+    const { id } = ctx.params;
+    //write에서 사용한 schema와 비슷한데, required()가 없다.
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    });
+
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true, //이 값을 설정하면 업데이트 된 데이터 반환
+            //false라면 업데이트 되기 전의 데이터를 반환
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
---
+++ blog-backend/src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import api from './api/index.js';
+
+// import createFakeData from './createFakeData.js';
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log('connected to mongoDB');
+        // createFakeData();
+    })
+    .catch(e => {
+        console.error(e);
+    });
+
+
+const app = new Koa();
+const router = new Router();
+
+//라우터 설정
+router.use('/api', api.routes());  //api 라우트 적용
+
+//라우터 적용 전에 bodyparser 적용
+app.use(bodyParser());
+
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log('listening to port %d', port);
+});
