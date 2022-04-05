7970757ad692558e05f6e2fa9c07fb7772e01514
e-7281998 <jejjcs43@naver.com>
2022-04-05 17:57:37 +0900

    회원인증 API - 로그인

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -63,6 +63,33 @@ export const register = async ctx => {
 
 export const login = async ctx => {
     //로그인
+    const { username, password } = ctx.request.body;
+
+    //username, password 없으면 에러처리
+    if (!username || !password) {
+        ctx.status = 401;   //Unauthorized ( 권한이 없는 )
+        return;
+    }
+
+    try {
+        const user = await User.findByUsername(username);
+        //계정이 없으면 에러처리
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+
+        //잘못된 비밀번호
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
 };
 export const check = async ctx => {
     //로그인 상태 확인
