897f3957ec6df919a20073e50341d3be6a87967d
e-7281998 <jejjcs43@naver.com>
2022-04-28 16:41:29 +0900

    로그인 상태 보이기

--- blog-frontend/src/components/common/Header.js
+++ blog-frontend/src/components/common/Header.js
@@ -32,9 +32,12 @@ const Wrapper = styled(Responsive)`
 const Spacer = styled.div`
     height: 4rem;
 `
+const UserInfo = styled.div`
+    font-weight: 800;
+    margin-right: 1rem;
+    `
 
-
-const Header = () => {
+const Header = ({ user }) => {
     return (
         <>
             <HeaderBlock>
@@ -42,9 +45,17 @@ const Header = () => {
                     <Link to="/" className="logo">
                         Reacters
                     </Link>
-                    <div className="right">
-                        <Button to="/login">로그인</Button>
-                    </div>
+                    {user ? (
+                        <div className='right'>
+                            <UserInfo>{user.username}</UserInfo>
+                            <Button>로그아웃</Button>
+                        </div>
+                    ) : (
+                        <div className="right">
+                            <Button to="/login">로그인</Button>
+                        </div>
+                    )}
+
                 </Wrapper>
             </HeaderBlock>
             <Spacer />
