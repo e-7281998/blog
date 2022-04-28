60615e3d429c57f0d1137311af09af009bae3a17
e-7281998 <jejjcs43@naver.com>
2022-04-28 17:11:47 +0900

    로그인 상태 유지

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -8,11 +8,25 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer, { rootSaga } from './modules';
 import createSagaMiddleware from '@redux-saga/core';
+import { tempSetUser, check } from './modules/user';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return; //로그인 상태가 아니라면 아무것도 안함
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
